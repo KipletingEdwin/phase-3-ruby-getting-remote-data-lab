70e72bd677f10bb419b85d1e1156483dd0d9285e
Kipleting <kipletingedwin4@gmail.com>
2022-08-28 22:08:52 +0300

    commit tree 829ddb68b8c53c5c516893e3d4fea09927ab9192

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,28 @@
 # Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+
+    attr_accessor:url
+
+    def initialize(url)
+        @url = url
+
+    end
+
+    def get_response_body
+        data = URI.parse(url)
+        response = Net::HTTP.get_response(data)
+        response.body
+
+    end
+
+    def parse_json
+        res = JSON.parse(self.get_response_body)
+        res.collect do |program|
+            program["agency"]
+        end
+    end
+end
